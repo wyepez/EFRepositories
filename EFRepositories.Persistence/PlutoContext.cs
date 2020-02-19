5695f77abed438bfa6338fa3923a17fa89ec4e15
Washington Xavier Yépez Yépez <wyepez@outlook.com>
2020-02-18 23:07:01 -0500

    Fixing problem "No Entity Framework provider found for the ADO.NET provider with invariant name 'System.Data.SqlClient'. Make sure the provider is registered in the 'entityFramework' section of the application config file. See http://go.microsoft.com/fwlink/?LinkId=260882 for more information."

--- EFRepositories.Persistence/PlutoContext.cs
+++ EFRepositories.Persistence/PlutoContext.cs
@@ -19,5 +19,14 @@ namespace EFRepositories.Persistence
             modelBuilder.Configurations.Add(new CourseConfiguration());
             modelBuilder.Configurations.Add(new TagConfiguration());
         }
+
+        private void FixEfProviderServicesProblem()
+        {
+            // The Entity Framework provider type 'System.Data.Entity.SqlServer.SqlProviderServices, EntityFramework.SqlServer'
+            // for the 'System.Data.SqlClient' ADO.NET provider could not be loaded. 
+            // Make sure the provider assembly is available to the running application. 
+            // See http://go.microsoft.com/fwlink/?LinkId=260882 for more information.
+            var instance = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+        }
     }
 }
